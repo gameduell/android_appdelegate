473b3ea58e6f7ccb81f313e3ecaf3edd96cb9352
jxav <joao.xavier@gameduell.de>
2015-03-09 10:42:01 +0100

    Simplified the backend to avoid the use of external class

--- android_appdelegate/AndroidAppDelegate.hx
+++ android_appdelegate/AndroidAppDelegate.hx
@@ -4,6 +4,7 @@
  */
 package android_appdelegate;
 
+import hxjni.JNI;
 import msignal.Signal;
 
 /**
@@ -11,8 +12,14 @@ import msignal.Signal;
 
     @author jxav
  */
-extern class AndroidAppDelegate
+class AndroidAppDelegate
 {
+    private static var initializeNative = JNI.createStaticMethod("org/haxe/duell/appdelegate/AppDelegate",
+    "initialize", "(Lorg/haxe/duell/hxjni/HaxeObject;)V");
+
+    // in reality this is a "forced" singleton, since we need to have the static initialization and not the lazy loading
+    private static var appDelegateInstance: AndroidAppDelegate = new AndroidAppDelegate();
+
     /**
         Called when the activity is starting.
      */
@@ -74,5 +81,72 @@ extern class AndroidAppDelegate
     /**
         Retrieves the instance of the app delegate.
      */
-    public static function instance(): AndroidAppDelegate;
+    public static inline function instance(): AndroidAppDelegate
+    {
+        return appDelegateInstance;
+    }
+
+    private function new(): Void
+    {
+        initializeNative(this);
+
+        onCreate = new Signal0();
+        onDestroy = new Signal0();
+        onPause = new Signal0();
+        onRestart = new Signal0();
+        onResume = new Signal0();
+        onStart = new Signal0();
+        onStop = new Signal0();
+        onTrimMemory = new Signal1<Int>();
+        onLowMemory = new Signal0();
+    }
+
+    //
+    // Java callbacks
+    //
+
+    public function create(): Void
+    {
+        onCreate.dispatch();
+    }
+
+    public function destroy(): Void
+    {
+        onDestroy.dispatch();
+    }
+
+    public function pause(): Void
+    {
+        onPause.dispatch();
+    }
+
+    public function restart(): Void
+    {
+        onRestart.dispatch();
+    }
+
+    public function resume(): Void
+    {
+        onResume.dispatch();
+    }
+
+    public function start(): Void
+    {
+        onStart.dispatch();
+    }
+
+    public function stop(): Void
+    {
+        onStop.dispatch();
+    }
+
+    public function lowMemory(): Void
+    {
+        onLowMemory.dispatch();
+    }
+
+    public function trimMemory(level: Int): Void
+    {
+        onTrimMemory.dispatch(level);
+    }
 }
